435f5f94527369e7e6b75a4aa8e7123415c576f1
JanaBaraka <145542559+JanaBaraka@users.noreply.github.com>
2024-04-15 00:04:30 +0200

    brute force function
    added a a function to randomly generate prime variables for p and q and added a brute force function to crack the value of d

--- bruteRSA.py
+++ bruteRSA.py
@@ -27,4 +27,30 @@ def generate_prime(bits):
         if is_prime(num):
             return num
         
-        
+def generate_keys(bits):
+    #generate p and q
+    p = generate_prime(bits)
+    q = generate_prime(bits)
+    n = p * q
+    phi = (p - 1) * (q - 1)
+    e = 2
+    #e must be coprime to phi
+    while gcd(e, phi) != 1:
+        e+=1
+
+    return (n, e), (p, q)
+
+def brute_force_d(n, e, c):
+    phi = (p - 1) * (q - 1)
+    d = 1
+    attempts = 0
+    while True:
+        attempts += 1
+        if (d * e) % phi == 1:
+            break
+        d += 1
+    return d, attempts
+
+
+
+
