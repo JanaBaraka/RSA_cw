59bd99509048c37708a95bb6aa8a24db521970df
JanaBaraka <145542559+JanaBaraka@users.noreply.github.com>
2024-04-17 20:40:48 +0200

    Update factorRSA.py

---
+++ factorRSA.py
@@ -0,0 +1,32 @@
+import math
+import random
+import time
+
+def gcd_extended(a, b):
+    if a == 0:
+        return b, 0, 1
+    gcd, x1, y1 = gcd_extended(b % a, a)
+    x = y1 - (b // a) * x1
+    y = x1
+    return gcd, x, y
+
+def modular_inverse(a, m):
+    gcd, x, y = gcd_extended(a, m)
+    if gcd != 1:
+        raise ValueError("Modular inverse does not exist")
+    return x % m
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
